51e2b5021484a71debe982793a2c3bb4b991c6f1
TheLoneliestTime <john.a.roxas@gmail.com>
2023-02-03 16:48:07 -0800

    wrote functionality for sum all numbers in array function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function sumItems(array) {
+const sumItems = function(array, index = 0, total = 0) {
   // Sum all the numbers in the array
-}
+
+
+
+  if (index - array.length === 0) {
+    return total;
+  } else {
+    if (Array.isArray(array[index])) {
+      array = array.flatMap(element => element);
+    }
+    return sumItems(array, index + 1, total = total + Number(array[index]));
+  }
+};
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
 
 module.exports = sumItems;
